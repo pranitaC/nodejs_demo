ad6e7187fe1fd4f8dcf118a7e9ed2720f630ff6f
Prashant Chaudhari <prashant@truespider.com>
2015-08-14 21:31:47 +0530

    error commit

--- app.js
+++ app.js
@@ -28,7 +28,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 app.use('/', site);
 app.use('/', users);
-app.use('/', products);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
